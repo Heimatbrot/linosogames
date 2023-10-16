c5330b800d9211c2e66eda0eb439ee7cae804231
Heimatbrot <114741802+Heimatbrot@users.noreply.github.com>
2023-10-16 09:45:56 +0200

    Update participants.js

--- src/javascript/participants.js
+++ src/javascript/participants.js
@@ -110,7 +110,7 @@ function searchTable() {
   }
 }
 
-var participants = [['Linus', 2, 0, 1, 1], ['Sean', 2, 0, 0, 1], ['Lara', 2, 1, 1, 0], ['Nicole', 2, 1, 0, 1], ['Simon', 2, 1, 1, 0], ['Caroline', 2, 0, 2, 0], ['Valerio', 2, 0, 0, 1], ['Marc', 2, 1, 0, 1], ['Sofia', 2, 1, 0, 0], ['Jana', 2, 0, 0, 1],['Gian', 0, 0, 0, 0],['Livio', 2, 1, 0, 0]];
+var participants = [['Linus', 2, 0, 1, 1], ['Sean', 2, 0, 0, 1], ['Lara', 2, 1, 1, 0], ['Nicole', 2, 1, 0, 1], ['Simon', 2, 1, 1, 0], ['Caroline', 2, 0, 2, 0], ['Valerio', 2, 0, 0, 1], ['Marc', 2, 1, 0, 1], ['Sofia', 2, 1, 0, 0], ['Jana', 2, 0, 0, 1],['Gian', 0, 0, 0, 0],['Livio', 2, 1, 0, 0],['Andri', 1, 0, 1, 0]];
 
 
 
